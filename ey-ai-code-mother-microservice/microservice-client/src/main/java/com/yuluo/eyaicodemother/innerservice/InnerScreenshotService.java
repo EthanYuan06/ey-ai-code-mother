8b6bc7a8b4eab8ddd2f222d75fe4de560ea1ef21
EthanYuan <heyuanshi.dev@outlook.com>
2026-09-13 10:53:31 +0800

    微服务改造 - 客户端模块 将用户服务、截图服务改造为仅内部调用的Inner服务 用户服务只选部分内部调用方法，其他如登录、注册均为外部调用无需迁移

---
+++ ey-ai-code-mother-microservice/microservice-client/src/main/java/com/yuluo/eyaicodemother/innerservice/InnerScreenshotService.java
@@ -0,0 +1,8 @@
+package com.yuluo.eyaicodemother.innerservice;
+
+/**
+ * 内部使用的截图服务
+ */
+public interface InnerScreenshotService {
+    String generateAndUploadScreenshot(String webUrl);
+}
---
+++ ey-ai-code-mother-microservice/microservice-client/src/main/java/com/yuluo/eyaicodemother/innerservice/InnerUserService.java
@@ -0,0 +1,37 @@
+package com.yuluo.eyaicodemother.innerservice;
+
+import com.mybatisflex.core.service.IService;
+import com.yuluo.eyaicodemother.exception.BusinessException;
+import com.yuluo.eyaicodemother.exception.ErrorCode;
+import com.yuluo.eyaicodemother.model.entity.User;
+import com.yuluo.eyaicodemother.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import static com.yuluo.eyaicodemother.constant.UserConstant.USER_LOGIN_STATE;
+
+/**
+ * 内部使用的用户服务
+ */
+public interface InnerUserService extends IService<User> {
+
+    List<User> listByIds(Collection<? extends Serializable> ids);
+
+    User getById(Serializable id);
+
+    UserVO getUserVO(User user);
+
+    // 静态方法，避免跨服务调用
+    static User getLoginUser(HttpServletRequest request) {
+        // 先判断用户是否登录
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        return currentUser;
+    }
+}
